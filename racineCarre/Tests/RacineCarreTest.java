a51470246a36a9851a4dea8dd008976929a04542
Unknown <at.frizzy@gmail.com>
2018-03-14 12:29:51 +0100

    testvalide

--- racineCarre/Tests/RacineCarreTest.java
+++ racineCarre/Tests/RacineCarreTest.java
@@ -1,39 +1,45 @@
 import org.junit.Test;
 
 import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class RacineCarreTest {
     @Test
-    public void testNegative(){
-        try{
-            RacineCarre.tableauRacine(-1,-1);
-            assertTrue("Noooooooon, les nombres négatifs Tanguy !",false);
-        }catch (Exception e){
-            assertTrue("Cool, t'as géré les nombres négatifs c:",true);
+    public void testNegative() {
+        try {
+            RacineCarre.tableauRacine(-1, -1);
+            assertTrue("Noooooooon, les nombres négatifs Tanguy !", false);
+        } catch (Error e) {
+            assertTrue("Cool, t'as géré les nombres négatifs c:", true);
         }
 
     }
+
     @Test
-    public void testInferieur(){
-        try{
-            RacineCarre.tableauRacine(-1,-1)[0];
-            assertTrue(":'(",false);
-        }catch (Exception e){
-            assertTrue("C'est bien :)",true);
+    public void testInferieur() {
+        try {
+            RacineCarre.tableauRacine(-1, -1);
+            assertTrue(":'(", false);
+        } catch (Error e) {
+            assertTrue("C'est bien :)", true);
         }
     }
-    public void testSingleValue(){
-        try{
-            assertEquals(RacineCarre.tableauRacine(4,4).length,1);
-        }catch (Exception e){
+
+    @Test
+    public void testSingleValue() {
+        try {
+            assertEquals(RacineCarre.tableauRacine(4, 4).length, 1);
+        } catch (Exception e) {
 
         }
     }
-    public void testValue(){
-        try{
-            assertEquals(RacineCarre.tableauRacine(4,4)[0],2);
-        }catch (Exception e){
+
+    @Test
+    public void testValue() {
+        try {
+            assertEquals(RacineCarre.tableauRacine(4, 5)[0], 2);
+        } catch (Exception e) {
 
         }
     }
