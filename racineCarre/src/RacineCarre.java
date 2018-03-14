c0a21016aafb847540e7b186c12fe74033856550
Unknown <at.frizzy@gmail.com>
2018-03-14 11:36:08 +0100

    repush

--- racineCarre/src/RacineCarre.java
+++ racineCarre/src/RacineCarre.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class RacineCarre {
+
+    public int[] tableauRacine(int A, int B) throws Exception {
+        if (B < A) {
+            throw new Exception("a");
+        }
+    }
+
 }
