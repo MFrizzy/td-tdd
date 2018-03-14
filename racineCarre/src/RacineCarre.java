b21fdb162f5f14f2a8c8d12f3d833466e846fcc4
Unknown <at.frizzy@gmail.com>
2018-03-14 11:47:33 +0100

    fin q1

--- racineCarre/src/RacineCarre.java
+++ racineCarre/src/RacineCarre.java
@@ -1,8 +1,21 @@
 public class RacineCarre {
 
-    public int[] tableauRacine(int A, int B) throws Exception {
-        if (B < A) {
-            throw new Exception("a");
+    public static int[] tableauRacine(int A, int B) throws Error {
+        if (B <= A) {
+            throw new Error("B <= A");
+        }
+        int taille = B - A;
+        int[] result = new int[taille];
+        for (int i = 0; i < taille; i++) {
+            result[i] = (int) Math.sqrt((double) B - i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] test = RacineCarre.tableauRacine(1, 10);
+        for (int i = 0; i < test.length; i++) {
+            System.out.println(test[i]);
         }
     }
 
