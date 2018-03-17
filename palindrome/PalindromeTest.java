d43db3a18ecfda224335ee580d870bac31d9cc17
Yves DANIEL <yves.daniel11@gmail.com>
2018-03-17 08:39:17 +0100

    Add files via upload

---
+++ palindrome/PalindromeTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class PalindromeTest {
+    @Test
+    public void randomCharsTest(){
+        Random r=new Random();
+        String str="";
+        String reverse="";
+        int value;
+        for(int i=0;i<10;++i){
+            value=r.nextInt(255);
+            str+=(char)value;
+            reverse=(char)value+reverse;
+        }
+        assertTrue(Palindrome.palindrome("")>0);
+    }
+    public void workTest(){
+        assertTrue(Palindrome.palindrome("non")==1);
+    }
+    public void multipleWordsTest(){
+        assertTrue(Palindrome.palindrome("non oui non Non")==3);
+    }
+}
